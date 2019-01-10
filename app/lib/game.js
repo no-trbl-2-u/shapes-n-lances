deec8d0359087770cd108f3371479a29576e29ee
David Rehmat <me@dave.blue>
2019-01-09 19:18:33 -0500

    handling the key press

--- app/lib/game.js
+++ app/lib/game.js
@@ -1,9 +1,11 @@
 ( (Game, Models, Engine) => {
-  
+
   const player = new Models.Entity("player", 10, 15, 100, 100);
 
-  Engine.Entities.push(player)
+    Engine.Entities.push(player);
+
 
 }) (window.Game = window.Game || {},
     window.Game.Models = window.Game.Models || {},
     window.Game.Engine = window.Game.Engine || {});
+
